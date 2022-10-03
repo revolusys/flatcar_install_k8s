e40c93ed61ee1deb9ec68145ca3e410d99d85991
revolusys <99296583+revolusys@users.noreply.github.com>
2022-10-03 15:53:15 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,13 @@ set -xe
 systemctl enable docker
 modprobe br_netfilter
 
+
+
 cat <<EOF | tee /etc/modules-load.d/k8s.conf
 br_netfilter
 EOF
+
+
 
 cat <<EOF | tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
@@ -22,25 +26,17 @@ RELEASE_VERSION="v0.14.0"
 DOWNLOAD_DIR=/opt/bin
 RELEASE="v1.23.6"
 
-mkdir -p /opt/cni/bin
+
+
+mkdir -p /opt/cni/bin && chmod -R u+wx /opt/bin
 mkdir -p /etc/systemd/system/kubelet.service.d
-
-curl() {
-	command curl -sSL "$@"
-}
-
-curl "https://github.com/containernetworking/plugins/releases/download/${CNI_VERSION}/cni-plugins-linux-amd64-${CNI_VERSION}.tgz" | tar -C /opt/cni/bin -xz
-curl "https://github.com/kubernetes-sigs/cri-tools/releases/download/${CRICTL_VERSION}/crictl-${CRICTL_VERSION}-linux-amd64.tar.gz" | tar -C $DOWNLOAD_DIR -xz
-curl "https://raw.githubusercontent.com/kubernetes/release/${RELEASE_VERSION}/cmd/kubepkg/templates/latest/deb/kubelet/lib/systemd/system/kubelet.service" | sed "s:/usr/bin:${DOWNLOAD_DIR}:g" | tee /etc/systemd/system/kubelet.service
-curl "https://raw.githubusercontent.com/kubernetes/release/${RELEASE_VERSION}/cmd/kubepkg/templates/latest/deb/kubeadm/10-kubeadm.conf" | sed "s:/usr/bin:${DOWNLOAD_DIR}:g" | tee /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
-curl --remote-name-all https://storage.googleapis.com/kubernetes-release/release/${RELEASE}/bin/linux/amd64/{kubeadm,kubelet,kubectl}
-
-curl -L --remote-name-all https://github.com/cilium/cilium-cli/releases/latest/download/cilium-linux-amd64.tar.gz{,.sha256sum}
-sha256sum --check cilium-linux-amd64.tar.gz.sha256sum
-sudo tar xzvfC cilium-linux-amd64.tar.gz /opt/bin
-rm cilium-linux-amd64.tar.gz{,.sha256sum}
+curl -sSL "https://github.com/containernetworking/plugins/releases/download/${CNI_VERSION}/cni-plugins-linux-amd64-${CNI_VERSION}.tgz" | tar -C /opt/cni/bin -xz
+curl -sSL "https://raw.githubusercontent.com/kubernetes/release/${RELEASE_VERSION}/cmd/kubepkg/templates/latest/deb/kubelet/lib/systemd/system/kubelet.service" | sed "s:/usr/bin:${DOWNLOAD_DIR}:g" | tee /etc/systemd/system/kubelet.service
+curl -sSL "https://raw.githubusercontent.com/kubernetes/release/${RELEASE_VERSION}/cmd/kubepkg/templates/latest/deb/kubeadm/10-kubeadm.conf" | sed "s:/usr/bin:${DOWNLOAD_DIR}:g" | tee /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+curl -sSL --remote-name-all https://storage.googleapis.com/kubernetes-release/release/${RELEASE}/bin/linux/amd64/{kubeadm,kubelet}
 
 chmod +x {kubeadm,kubelet,kubectl}
-mv {kubeadm,kubelet,kubectl} $DOWNLOAD_DIR/
+mv {kubeadm,kubelet} $DOWNLOAD_DIR/
 
-systemctl enable --now kubelet
+systemctl enable --now kubelet && systemctl start kubelet
+systemctl status kubelet
